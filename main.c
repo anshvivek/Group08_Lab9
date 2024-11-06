21d4422e18383a428f0703188169f46db44ccd23
anshvivek <104276633+anshvivek@users.noreply.github.com>
2024-11-06 13:55:16 +0530

    sawtooth

--- main.c
+++ main.c
@@ -2,7 +2,10 @@
 #include <stdbool.h>
 #include "tm4c123gh6pm.h"
 
-void I2CSlaveHandler();
+void ISR();
+// void I2CSlaveHandler();
+
+uint32_t TxData = 0x0000;
 
 int main(void)
 {
@@ -38,23 +41,33 @@ int main(void)
     I2C1_SCSR_R = 0x01;
     I2C1_SIMR_R = 0x01; // interrupt enabled for data rx at slave
 
+    NVIC_ST_RELOAD_R = 16000; // 1 millisecond
+    NVIC_ST_CURRENT_R = 0x00;
+    NVIC_ST_CTRL_R = 0x00000007;
+	
     // master tx
     I2C0_MCR_R = 0x10;
     I2C0_MTPR_R = 0x09;
     I2C0_MSA_R = 0x00;// initializing
 
-    uint32_t TxData = 0x0008;
+    // uint32_t TxData = 0x0008;
     uint8_t slave_address = 0x60;
 	
-    DAC_Tx(TxData, 2, slave_address);
+    
 	
     while(1){
-        ; }
+        DAC_Tx(TxData, 2, slave_address);
+    }
     return 0;
 }
 
 
 void DAC_Tx(uint32_t data, int no_of_bytes, uint8_t s_address){
+    
+    uint32_t Least_Sig_Byte = (data & 0xFF);
+    data = data >> 8;
+    data |= (Least_Sig_Byte << 8);
+
     int sent_bytes = 0;
     I2C0_MSA_R = (s_address << 1);
     I2C0_MDR_R = (data & 0xFF);
@@ -97,3 +110,11 @@ void I2CSlaveHandler(){
         GPIO_PORTF_DATA_R = 0x04;}    }
     I2C1_SICR_R = 0x01;
 }
+void ISR(){
+    GPIO_PORTF_DATA_R ^= 0x04;
+    TxData += 10;
+    if(TxData >= 4096){
+        TxData = 0;
+    }
+}
+
